0e10db4d7217a927e21c9742474ac87faa29b2d5
bao-dimage <bao-nguyen@dimage.co.jp>
2024-06-25 14:06:17 +0700

    fix database

--- database/seeders/ReviewSeeder.php
+++ database/seeders/ReviewSeeder.php
@@ -2,26 +2,31 @@
 
 namespace Database\Seeders;
 
-use App\Models\Review;
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
+use App\Models\User;
+use App\Models\Review;
+use Faker\Factory as Faker;
 
 class ReviewSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
     public function run(): void
     {
+        $faker = Faker::create('vi_VN'); 
+
         $userIds = User::pluck('id')->all();
 
-        Review::factory()->count(60)->create()->each(function ($review) use ($userIds) {
-            $review->likedUsers()->attach(Arr::random(
-                $userIds,
-                rand(0, 5)
-            ));
+        Review::factory()->count(60)->create()->each(function ($review) use ($userIds, $faker) {
+            $content = $faker->realText(250); 
+            $review->content = $content;
+            $review->save();
+
+            $review->likedUsers()->attach(Arr::random($userIds, rand(0, 5)));
         });
     }
 }

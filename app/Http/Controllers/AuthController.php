5d24e94ff6da6d92e70bfc0cf145ca54ca251d1f
bao-dimage <bao-nguyen@dimage.co.jp>
2024-06-26 00:12:54 +0700

    aws s3

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,7 +38,7 @@ class AuthController extends Controller
     
         $avatar = $request->file("avatar");
         if (!empty($avatar)) {
-            $storedPath= Storage::disk('s3')->put('avatars', $avatar[0],'public');
+            $storedPath= Storage::disk('s3')->put('avatars', $avatar[0]);
             $user->avatar = Storage::url($storedPath);
             $user->save();
         }

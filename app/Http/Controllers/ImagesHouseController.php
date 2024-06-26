2743de4f58452018500b11e9b0f0d77b2840a484
bao-dimage <bao-nguyen@dimage.co.jp>
2024-06-26 09:56:14 +0700

    aws s3

--- app/Http/Controllers/ImagesHouseController.php
+++ app/Http/Controllers/ImagesHouseController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
+
 class ImagesHouseController extends Controller
 {
     public function storeImagesHouse($request, $house)
@@ -14,9 +16,10 @@ class ImagesHouseController extends Controller
             $count = 0;
             foreach ($imageFiles as $image) {
                 // Lưu file vào storage/app/public/image -> file được link tới thư mục public/storage/image
-                $imageOriginalExtension = 'House_' . $house->id . '_' . $count . '.' . $image->getClientOriginalExtension();
-                $url = 'image/'.$house->id;
-                $imageUrl = $image->storeAs($url, $imageOriginalExtension, 'public');
+                // $imageOriginalExtension = 'House_' . $house->id . '_' . $count . '.' . $image->getClientOriginalExtension();
+                // $url = 'image/'.$house->id;
+                // $imageUrl = $image->storeAs($url, $imageOriginalExtension, 'public');
+                $imageUrl = Storage::disk('s3')->put('houses', $image);
                 $house->images()->create(['url' => $imageUrl]);
                 $count++;
             }

99c3295ef131be4fc1d7d1efd15d9ccb0650b006
bao-dimage <bao-nguyen@dimage.co.jp>
2024-06-26 10:16:50 +0700

    aws s3

--- app/Http/Controllers/ImagesHouseController.php
+++ app/Http/Controllers/ImagesHouseController.php
@@ -15,12 +15,8 @@ class ImagesHouseController extends Controller
 
             $count = 0;
             foreach ($imageFiles as $image) {
-                // Lưu file vào storage/app/public/image -> file được link tới thư mục public/storage/image
-                // $imageOriginalExtension = 'House_' . $house->id . '_' . $count . '.' . $image->getClientOriginalExtension();
-                // $url = 'image/'.$house->id;
-                // $imageUrl = $image->storeAs($url, $imageOriginalExtension, 'public');
                 $imageUrl = Storage::disk('s3')->put('houses', $image);
-                $house->images()->create(['url' => $imageUrl]);
+                $house->images()->create(['url' => Storage::url($imageUrl)]);
                 $count++;
             }
         }
